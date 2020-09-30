4a39c66a933d5e8aad62647e95be2962ceaab9f4
sylvainEmm <sylvainmakak@gmail.com>
2020-09-30 16:23:59 +0000

    Update 300122014.py

--- 3.Variables/300122014.py
+++ 3.Variables/300122014.py
@@ -4,5 +4,11 @@ ID 300122014
 
 ceci est mon premier programme
 """
-
+S="BONJOUR TOUT LE MONDE!"
 print ("bonjour le monde\n")
+print ("chaine de caractere",S)
+i=1
+print ("entier",1)
+
+D=4.5
+print ("reel",D)
