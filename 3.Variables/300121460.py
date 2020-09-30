7d47a55a5c8d2cb0e83e500b554da8730e98f283
Daveinfo20 <henryinuka96@gmail.com>
2020-09-30 16:31:28 +0000

    Update 300121460.py

--- 3.Variables/300121460.py
+++ 3.Variables/300121460.py
@@ -7,4 +7,10 @@ Created on Wed Sep 30 10:29:10 2020
 ID:300121460
 
 var="Bonjour tout le monde!"
-print(var)
+print("Chaine de caractère:", var)
+
+plus=1
+print("C'est un entier:", plus)
+
+imaginaire=4.6
+print("C'est un réel:", imaginaire)
