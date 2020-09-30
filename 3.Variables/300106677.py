483bfa5e83f4f08240ed2d35ad2250ef9c7001c8
Leonaldo1994 <leonaldo_decimus@yahoo.com>
2020-09-30 16:22:31 +0000

    Add new file

---
+++ 3.Variables/300106677.py
@@ -0,0 +1,15 @@
+# -*- coding: utfh-8 -*-
+"""
+ID: 300106677
+
+structure de variable 
+"""
+
+s= "Bonjour le monde" 
+print ("chaine de caractere", s)
+ 
+i=1
+print (" entier, i")
+
+d= 4.6
+print (" reel" , d)
