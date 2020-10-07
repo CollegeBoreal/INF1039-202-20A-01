8143b8d240ccb2cabfc8d996e3f17cfc07a51a25
Daveinfo20 <henryinuka96@gmail.com>
2020-10-07 14:50:34 +0000

    Update 300121460.py

--- 3.Variables/300121460.py
+++ 3.Variables/300121460.py
@@ -14,3 +14,24 @@ print("C'est un entier:", plus)
 
 imaginaire=4.6
 print("C'est un réel:", imaginaire)
+
+#Pour faire le calcul avec un discriminant réduit
+
+from math import sqrt
+a=float(input("Entrer la valeur de a:"))
+print(a)
+b=float(input("Entrer la valeur de b:"))
+print(b)
+c=float(input("Entrer la valeur de c:"))
+print(c)
+delta=float(b**2-4*a*c)
+print("La valeur du calcul de delta est:", delta)
+if (delta<0):
+    
+    print("Pas de solution")
+elif (delta==0):
+    print("Il y a une seule solution:", -b/(2*a))
+else:
+    racine=sqrt(delta)
+    print("\nLa racine de delta est:", racine)
+    print("\nIl y a deux racines:", (-b-racine)/(2*a), (-b+racine)/(2*a))
