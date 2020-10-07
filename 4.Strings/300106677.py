aae97e2b664763d4d57d55eab4b79f008e02cda7
Leonaldo1994 <leonaldo_decimus@yahoo.com>
2020-10-07 14:46:30 +0000

    mon programme sur les chaines de caractere

---
+++ 4.Strings/300106677.py
@@ -0,0 +1,21 @@
+# -*- coding: utfh-8 -*-
+"""
+ID: 300106677
+
+structure de variable 
+"""
+
+s= "Bonjour le monde" 
+print ("chaine de caractere", s)
+ 
+i=1
+print (" entier", i)
+
+d= 4.6
+aprint (" reel" , d)
+
+nom = ''college''
+concanation
+
+longueur = len(nom)
+print (nom chaine de caractere + + 'nom de ' )
