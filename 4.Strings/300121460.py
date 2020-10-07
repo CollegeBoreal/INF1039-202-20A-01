c047c479651380eaa310858902cb8267aa78ed6d
Daveinfo20 <henryinuka96@gmail.com>
2020-10-07 15:02:58 +0000

    Add new file

---
+++ 4.Strings/300121460.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Oct  7 10:46:38 2020
+
+@author: user
+"""
+
+#Chaine de caractère et ses opérations
+
+allo='a'
+print("Le caractère est:\n", allo)
+hola="Bonjour à tous!"
+print("La chaine de caractère est:\n", hola)
+
+#Longueur d'une chaine de caractere
+
+calcul="Bienvenue dans le cours 1042!"
+
+long=len(calcul)
+print("La longueur de la chaine est:\n", long)
+
+#Concatenation
+
+print("La concatenation des chaines est:\n", hola + " "  + calcul)
