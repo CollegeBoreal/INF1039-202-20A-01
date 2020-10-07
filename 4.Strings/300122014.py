2981d0dcef8c1f168b64874a20b671c2b678171f
sylvainEmm <sylvainmakak@gmail.com>
2020-10-07 14:48:57 +0000

    Add new file

---
+++ 4.Strings/300122014.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+"""
+ID: 300122014
+
+pratique de Strings 
+"""
+# pour un caractere 
+c = 'a'
+
+print(c)
+# pour chaine de caractere
+s = "Bonjour"
+
+print(s)
+
+nom = "Boreal"
+
+# pour la Concatenation
+
+print(s + " " + nom)
+
+# pour la longueur de la chaine de charactere - fonction
+
+longueur = len(nom)
+
+print(longueur)
+
+
+
+
