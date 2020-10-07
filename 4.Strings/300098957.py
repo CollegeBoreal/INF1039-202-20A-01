e631c72417ab0b0a36b6c78a910bbc93d76a6735
Brice Robert <7077779-b300098957@users.noreply.gitlab.com>
2020-10-07 14:21:54 +0000

    Add new file

---
+++ 4.Strings/300098957.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+"""
+ID: 300098957
+
+Strings 
+"""
+
+c = 'a'
+
+print(c)
+
+s = "Bonjour"
+
+print(s)
+
+nom = "Boreal"
+
+# Concatenation
+
+print(s + " " + nom)
+
+# longueur de la chaine de charactere - fonction
+
+longueur = len(nom)
+
+print(longueur)
+
+
+# f(x) = x + 1 - fonction algebraique - AlhKwarizmi
+
