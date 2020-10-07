46357684fe8057f8ce556e7d90e66ca3069b4ca4
steve Ebengue <euniceebengue20@gmail.com>
2020-10-07 15:20:35 +0000

    Add new file

---
+++ 4.Strings/300122131.py
@@ -0,0 +1,29 @@
+# -*- coding: utf-8 -*-
+"""
+ID: 300122131
+
+strings
+"""
+
+c = 'a'
+
+print(c)
+
+s = "bonjour"
+
+print (s)
+
+nom = "boreal"
+
+# concatenation
+
+print(s + " " + nom)
+
+# longueur de la chaine de charactere = fonction
+
+longueur = len(nom)
+
+print(longueur)
+
+
+# f(x) = x + 1 - fonction algebraique - alhkwarizmi
