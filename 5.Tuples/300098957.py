e319121291c7852b85969375d0fec312ff5717a1
Brice Robert <7077779-b300098957@users.noreply.gitlab.com>
2020-10-14 15:33:25 +0000

    Add new file

---
+++ 5.Tuples/300098957.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Oct 14 11:17:56 2020
+
+@author: 300098957
+"""
+
+a = 'A'
+print(a)
+
+etudiant = ( "Brice", 300098957, "Enseignant")
+print(etudiant[2])
