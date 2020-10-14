248977c696f68f1664631507c44eecf9145e6717
Sio-Mh <mahancyrille@yahoo.fr>
2020-10-14 16:11:31 +0000

    Update 300117811.py

--- 5.Tuples/300117811.py
+++ 5.Tuples/300117811.py
@@ -1,9 +1,9 @@
-a='A'
- print (a)
- etudiant =("cyrille",300122014,"enseignant")
- print(etudiant[0])
+a ='A'
+print (a)
+etudiant =("cyrille",300122014,"enseignant")
+print(etudiant[0])
 cyrille
- print(etudiant[1])
+print(etudiant[1])
 300117811
- print(etudiant[2])
+print(etudiant[2])
 enseignant
