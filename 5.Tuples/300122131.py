6d9e513bd962450546322df747ec7373063d97ed
steve Ebengue <euniceebengue20@gmail.com>
2020-10-14 15:41:24 +0000

    Update 300122131.py

--- 5.Tuples/300122131.py
+++ 5.Tuples/300122131.py
@@ -1,12 +1,9 @@
 Python 3.8.6 (tags/v3.8.6:db45529, Sep 23 2020, 15:52:53) [MSC v.1927 64 bit (AMD64)] on win32
 Type "help", "copyright", "credits" or "license" for more information.
 >>> etudiant = ("steve", 300122131)
->>>
 >>> print(etudiant[0])
-steve
+
 >>> print(etudiant[1])
-300122131
+
 >>> etudiant = ("brice", 300122131, "enseignant")
 >>> print(etudiant[2])
-enseignant
->>>
