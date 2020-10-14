3ff34674ed8d22dc54ddd8615b88da2c7145a39f
Daveinfo20 <henryinuka96@gmail.com>
2020-10-14 16:04:42 +0000

    Add new file

---
+++ 5.Tuples/300121460.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+"""
+Created on %(10-10-2020)s
+
+@author: %(Davidson)s
+"""
+
+étudiant=("Davidson Henry", 300121460)
+print("L'affichage du tuple est:\n", étudiant)
+print("L'affichage de l'indice 0 de mon tuple est:\n", étudiant[0])
+print("L'affichage de l'indice 1 de mon tuple est:\n", étudiant[1])
