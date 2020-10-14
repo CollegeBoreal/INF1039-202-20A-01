fbb5fe2349c4faf99b98a22cf45b73f58fe262c8
steve Ebengue <euniceebengue20@gmail.com>
2020-10-14 15:40:17 +0000

    Add new file

---
+++ 1.Capstone-I/300122131.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+"""
+# -*- coding: utf-8 -*-
+"""
+#id 300122131
+"mot de bienvenue"
+
+s= "bienvenue dans mon programme de conversion"
+print(s)
+
+#recuperation du nombre de minute
+a=float(input("entrer le nombre de minute"))
+#calcul de lheure
+b=a/60
+#affichage de l'heure partie entiere
+heure=int(b)
+#calcul de l'heure
+c=b-heure
+#afficher les minutes en arrondi
+minute=round(c*60)
+#ecrire le resultat a lutilisateur
+x="heure(s)"
+y="minute(s)"
