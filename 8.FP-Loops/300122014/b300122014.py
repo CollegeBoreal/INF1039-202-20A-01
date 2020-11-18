1d7b0c25df4943b222358a871d6435864c965dd7
sylvain <sylvainmakak@gmail.com>
2020-11-18 12:37:56 -0500

    mon programme en python:snake:

---
+++ 8.FP-Loops/300122014/b300122014.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+"""
+Created on <12:20:00>
+
+@author: <sylvain emmanuel >
+"""
+
+def fonction_1():
+   return 0
+   
+def main():
+   fonction_1()
+   
+if __name__ == '__main__':
+    main()
