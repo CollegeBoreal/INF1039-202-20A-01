bfab9461bdecd508e27b6e852e9739cf47c773cd
Sio-Mh <71027809+Sio-Mh@users.noreply.github.com>
2020-11-18 09:54:30 -0500

    Create 300117811.py

---
+++ 3.Variables/300117811.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+"""
+ID 300117811
+
+structure 
+
+"""
+
+S="BONJOUR BOREAL"
+print ("bonjour boreal\n")
+print ("chaine de caractere",S)
+i=1
+print ("entier",1)
+
+D=4.5
+print ("reel",D)
