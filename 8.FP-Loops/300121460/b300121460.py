bcce2813f5e135e495551e8a30790d1d551c5e95
Daveinfo20 <henryinuka96@gmail.com>
2020-11-18 10:36:27 -0500

    Hola!

---
+++ 8.FP-Loops/300121460/b300121460.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+"""
+Created on  2020-11-18 10:21:00
+
+@author: Davidson Henry 
+"""
+
+def fonction_1():
+   return 0
+   
+def main():
+   fonction_1()
+   
+if __name__ == '__main__':
+    main()
