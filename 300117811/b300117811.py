70fe6e04d657af446433e88e1e60d0458833cdf3
Sio_mh <mahancyrille@yahoo.fr>
2020-11-18 12:11:58 -0500

    Mon emoji  :+1:

---
+++ 300117811/b300117811.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+"""
+Created on 18-11-2020 11:42
+
+@author: 300117811
+"""
+
+def fonction_1():
+   return 0
+   
+def main():
+   fonction_1()
+   
+if __name__ == '__main__':
+    main()
+
