2e23568b904f38e9e4ec04df1981cd8c976266c2
Brice <62551735+b300098957@users.noreply.github.com>
2020-12-01 14:35:23 -0500

    Update notation.sh

--- 7.Functions/.scripts/notation.sh
+++ 7.Functions/.scripts/notation.sh
@@ -24,13 +24,14 @@ echo ""
 echo "|:hash:| Boréal :id:                |  Notation         |"
 echo "|------|----------------------------|-------------------|"
 
-i=1
+i=0
 
 for id in "${ETUDIANTS[@]}"
 do
+   U_AVATAR="<image src='https://avatars0.githubusercontent.com/u/${AVATARS[$i]}?s=460&v=4' width=20 height=20></image>"
    FILE=b${id}.py
-   OK="| ${i} | [${id}](../${FILE}) | [:tada:](Execution.md#etudiant-${id}) | "
-   KO="| ${i} | [${id}](../${FILE}) | [:x:](Execution.md#etudiant-${id}) | "
+   OK="| ${i} | [${id}](../${FILE}) - ${U_AVATAR} | [:tada:](Execution.md#etudiant-${id}) | "
+   KO="| ${i} | [${id}](../${FILE}) - ${U_AVATAR} | [:x:](Execution.md#etudiant-${id}) | "
    if [ -f "$FILE" ]; then
       pytest .scripts/b${id}0000.py 2>&1 >  /dev/null
       RES=`echo $?`
