41b92a7ca3c0376fe1538e9fc90944a485fc61dd
itonga <73952068+itonga@users.noreply.github.com>
2020-12-02 03:21:38 -0500

    Create 300115206.py

---
+++ 8. boucles FP/300115206/300115206.py
@@ -0,0 +1,14 @@
+# - * - codage: utf-8 - * -
+"" "
+Créé le 2020-11-18 10:45:00
+@auteur: jojo
+"" "
+
+def  fonction_1 ():
+   retourne  0
+   
+def  main ():
+   fonction_1 ()
+   
+si  __nom__  ==  '__main__' :
+    principal ()
