d5908b6d25bb2ef184a09e528837d9fa6fcc214c
sylvainEmm <71392439+sylvainEmm@users.noreply.github.com>
2020-12-02 11:04:13 -0500

    Update 300122014.py

--- 3.Variables/300122014.py
+++ 3.Variables/300122014.py
@@ -1,7 +1,6 @@
 # -*- coding: utf-8 -*-
 """
 ID 300122014
-
 ceci est mon premier programme
 """
 S="BONJOUR TOUT LE MONDE!"
@@ -12,3 +11,20 @@ print ("entier",1)
 
 D=4.5
 print ("reel",D)
+
+
+
+
+#nom et tel du collège
+n="collège boreal"
+print ("nom du collège",n)
+tel=5144041822
+print ("numéro",tel)
+
+#programme d'etude et diplome
+p= "technique du genie informatique"
+
+print ("programme d'etude",p)
+
+d="DEC: Diplome d'étude collégiale"
+print ("diplome",d)
