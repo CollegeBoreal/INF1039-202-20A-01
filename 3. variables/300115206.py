214b7f5af33d937665e628b2f6edd997f05bbdb8
itonga <73952068+itonga@users.noreply.github.com>
2020-12-02 02:57:23 -0500

    variable

---
+++ 3. variables/300115206.py
@@ -0,0 +1,17 @@
+# Mon programme
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Dec  2 02:55:54 2020
+
+@author: Jojo
+"""
+
+#structure de donnees: variable
+S =  "Bonjour tout le monde"
+print ( "Chaine de caractere" , S )
+
+i =  1
+imprimer ( "Entier" , i )
+
+d =  4,6
+imprimer ( "Reel" , d )
