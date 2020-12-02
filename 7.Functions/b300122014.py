7ef0a4cc43359b582cb58e7a9e0e10c3faf2f60e
sylvainEmm <71392439+sylvainEmm@users.noreply.github.com>
2020-12-02 11:55:50 -0500

    Update b300122014.py

--- 7.Functions/b300122014.py
+++ 7.Functions/b300122014.py
@@ -3,3 +3,20 @@ def add_sub(n1,n2):
     sub = n1 - n2
     return (add,sub)
 (a,b) = add_sub(3,4)
+print ((a,b))
+
+
+(a,b)=add_sub(10,20)
+print((a,b))
+
+(a,b)=add_sub(15,45)
+print((a,b))
+
+(a,b)=add_sub(25,75)
+print((a,b))
+
+(a,b)=add_sub(48,69)
+print((a,b))
+
+(a,b)=add_sub(70,110)
+print((a,b))
