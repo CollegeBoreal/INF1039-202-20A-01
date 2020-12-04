5de90117d9b3d8f9aca4a89c4da7d81aef163e33
Naoufal271 <72874987+Naoufal271@users.noreply.github.com>
2020-12-04 22:12:23 +0100

    Update 300115064.py

--- 7.Functions/300115064.py
+++ 7.Functions/300115064.py
@@ -1,2 +1,15 @@
-def add_sub():
-   return 1;
+def func1():
+    print("i am a fuction")
+    
+def func2(arg1, arg2):
+    print(arg1," ", arg2)
+def cube(x):
+    return x*x*x
+def power(num, x=1):
+    result = 1
+    for i in range (x):
+        result = result * num
+    return result
+    func2(10,20)
+print(func2(10,20))
+print(cube(3))
