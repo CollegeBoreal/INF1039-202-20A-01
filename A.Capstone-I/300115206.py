e31d2e67e99a3b636b6d848d88dea83ae716f1af
itonga <73952068+itonga@users.noreply.github.com>
2020-12-06 22:59:12 -0500

    Update 300115206.py

--- A.Capstone-I/300115206.py
+++ A.Capstone-I/300115206.py
@@ -1,1 +1,19 @@
-print: salut
+
+
+
+minutes_to_convert=123
+hours_decimal=minutes_to_convert/60
+hours_part=int(hours_decimal)
+
+minutes_decimal=hours_decimal-hours_part
+minutes_part=round(minutes_decimal*60)
+
+print("******************")
+
+print("The hour is:\n", hours_part)
+
+print("******************")
+
+print("The minutes are:\n", minutes_part)
+
+print("******************")
