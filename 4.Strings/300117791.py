e67c2758c6b2ece22f56ea4974e8df741c419b14
donmise <73952191+donmise@users.noreply.github.com>
2020-12-09 07:22:57 -0500

    Update 300117791.py

--- 4.Strings/300117791.py
+++ 4.Strings/300117791.py
@@ -1,7 +1,30 @@
-# -*- coding: utf-8 -*-
-"""
-Spyder Editor
+c = 'a'
 
-This is a temporary script file.
-"""
-print(1)
+print(c)
+
+s = "Bonjour"
+
+print(s)
+
+nom = "Boreal"
+
+# Concatenation
+
+print(s + " " + nom)
+
+# longueur de la chaine de charactere - fonction
+
+longueur = len(nom)
+
+print(longueur)
+
+
+ 
+c =' bonsoir'
+print (c)
+
+canada = "Toronto"
+I ="300117791"
+print (canada+" "+I)
+
+longeur = name(Misé)
