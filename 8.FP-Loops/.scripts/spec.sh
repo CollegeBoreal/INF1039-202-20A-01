b35d7a378da9a694a657ef667b441d1b2fd96f3c
Brice <62551735+b300098957@users.noreply.github.com>
2020-12-10 22:31:21 -0500

    Update spec.sh

--- 8.FP-Loops/.scripts/spec.sh
+++ 8.FP-Loops/.scripts/spec.sh
@@ -7,15 +7,16 @@
 # --------------------------------------
 
 generate_spec () {
-   echo "import sys; sys.path.append('.') # Rajouter le repertoire courant" > .scripts/b${id}0000.py
-   echo "import pytest" >> .scripts/b${id}0000.py
+   index="0000"
+   echo "import sys; sys.path.append('.') # Rajouter le repertoire courant" > .scripts/b${id}${index}.py
+   echo "import pytest" >> .scripts/b${id}${index}.py
    echo " " >> .scripts/b${id}0000.py
-   echo "@pytest.fixture" >> .scripts/b${id}0000.py
-   echo "def bypass():" >> .scripts/b${id}0000.py
-   echo "   from b${id} import fonction_1" >> .scripts/b${id}0000.py
-   echo "   return fonction_1() " >> .scripts/b${id}0000.py
-   echo " " >> .scripts/b${id}0000.py
-   echo "def test_answer(bypass):" >> .scripts/b${id}0000.py
-   echo "   assert bypass == [0, 2, 4, 6, 8, 10, 12, 14, 16, 18, 20]" >> .scripts/b${id}0000.py
+   echo "@pytest.fixture" >> .scripts/b${id}${index}.py
+   echo "def bypass():" >> .scripts/b${id}${index}.py
+   echo "   from b${id} import fonction_1" >> .scripts/b${id}${index}.py
+   echo "   return fonction_1() " >> .scripts/b${id}${index}.py
+   echo " " >> .scripts/b${id}${index}.py
+   echo "def test_answer(bypass):" >> .scripts/b${id}${index}.py
+   echo "   assert bypass == [0, 2, 4, 6, 8, 10, 12, 14, 16, 18, 20]" >> .scripts/b${id}${index}.py
 }
 
